1360405c0484b7dd32f28454ab53a4e1a51a6d2f
Anastasya <okayota83@mail.ru>
2022-12-18 14:54:58 +0400

    correction Task_2.1

--- Task_2.1.py
+++ Task_2.1.py
@@ -5,7 +5,7 @@
 number = float (input('Введите число - '))
 if number < 0:
       number*= -1
-while number % 10 != 0 : 
+while number % 1 != 0 : 
        number = number * 10
 new_number = int (number)
 sum = 0
