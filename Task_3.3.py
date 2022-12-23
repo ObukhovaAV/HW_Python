198b9a1acf212e473a44243d789c963970d510f7
Anastasya <okayota83@mail.ru>
2022-12-23 21:26:20 +0400

    change Task_3.3

--- Task_3.3.py
+++ Task_3.3.py
@@ -1,11 +1,14 @@
 #3. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 #Без использования встроенной функции преобразования, без строк.
 
-number = int (input ('Введите число - '))
-my_list = []
-while number:
-    number_2 = number % 2
-    number = number // 2
-    my_list.append (number_2)
-my_list.reverse ()
+def num_trans (number):
+    my_list = []
+    while number:
+        number_2 = number % 2
+        number = number // 2
+        my_list.append (number_2)
+    my_list.reverse ()
+    return my_list
+
+my_list = num_trans(int(input('Введите число - ')))
 print ('Число в двоичном исчислении = ', "".join(map(str, my_list))) 
